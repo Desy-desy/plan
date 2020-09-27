18b9a73e1b7785ca1ef7c9b1805b29e7d7cb149b
Desy-desy <136327293@qq.com>
2020-09-27 19:48:34 +0800

    kang

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header/>
+    <HeaderNav></HeaderNav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import HeaderNav from '@/components/HeaderNav.vue'
+
+export default {
+  components: {
+    Header,
+    HeaderNav,
+  }
+}
+</script>
