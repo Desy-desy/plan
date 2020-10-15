0ce1e2d39290b2b95de331abe50abd205b13ce6d
[Desy] <[136327293@qq.com]>
2020-10-15 19:09:21 +0800

    feedback

--- node.js/express_feedback/app.js
+++ node.js/express_feedback/app.js
@@ -1,6 +1,10 @@
+let bodyParser = require('body-parser')
 let express = require('express')
 
 let app = express()
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
 
 app.use('/public/',express.static('./public/'))
 
@@ -33,9 +37,22 @@ app.get('/post',(req,res)=>{
   res.render('post.html')
 })
 
-app.get('/pinglun',(req,res)=>{
-  let comment = req.query
-  comment.dateTime = '2020-02-02 10:11:12'
+// app.get('/pinglun',(req,res)=>{
+//   let comment = req.query
+//   comment.dateTime = '2020-02-02 10:11:12'
+//   comments.unshift(comment)
+//   res.redirect('/')
+// })
+
+//当以post请求/post的时候，执行指定的处理函数
+//可以让不同的请求方法让一个路径使用多次
+app.post('/post',(req,res)=>{
+  // console.log('收到表单post请求了')
+  //获取post请求数据，处理，发送响应
+  //req.query只能拿get请求参数
+  //配置body-parser 中间件（插件，专门来解析表单post请求体）
+  let comment = req.body
+  comment.dateTime = '2020-02-02 20:20:20'
   comments.unshift(comment)
   res.redirect('/')
 })
