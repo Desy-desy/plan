2d57c71aafa2666f45a216484147c47b012dfdf1
[Desy] <[136327293@qq.com]>
2020-10-19 09:08:30 +0800

    ajax封装

--- node.js/11-回调函数.js
+++ node.js/11-回调函数.js
@@ -1,12 +1,26 @@
 function add (x,y,callback) {
-  console.log(1);
+  console.log(1)
   setTimeout(function () {
     var ret = x + y
     callback(ret)
+    console.log(2)
   },1000)
 }
 
 add(10,20,function (ret) {
   console.log(ret)
 })
+console.log(3)
 
+// console.log(add(10,20))  ==>undefined
+
+
+// function add (x,y) {
+//   console.log(1)
+//   setTimeout(function () {
+//     var ret = x + y
+//     console.log(ret)
+//   },1000)
+// }
+
+// console.log(add(10,20));
